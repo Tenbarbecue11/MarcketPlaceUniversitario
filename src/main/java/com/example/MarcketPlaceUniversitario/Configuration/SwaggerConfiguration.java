ac0d45c1ecdd55453e447dda49f61190579c32cb
Tomás Poveda <tomaspoveda11@gmail.com>
2026-05-11 15:32:34 -0500

    retorno a la ultima verción estable

--- src/main/java/com/example/MarcketPlaceUniversitario/Configuration/SwaggerConfiguration.java
+++ src/main/java/com/example/MarcketPlaceUniversitario/Configuration/SwaggerConfiguration.java
@@ -2,11 +2,9 @@ package com.example.MarcketPlaceUniversitario.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -14,14 +12,7 @@ public class SwaggerConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
 
-        Server server = new Server();
-
-        server.setUrl(
-                "https://superb-curiosity-production-a149.up.railway.app"
-        );
-
         return new OpenAPI()
-                .servers(List.of(server))
                 .info(new Info()
                         .title("Marcket Place Universitario")
                         .version("1.0")
